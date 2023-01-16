62f47ec72eabebaad27d822b7134221d274ed4e1
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-01-16 22:34:37 +0800

    Moved into a new folder structure

--- Test/Test.java
+++ Test/Test.java
@@ -3,57 +3,104 @@ import static java.lang.Math.*;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) {
 
-        //Declaring a variable for switch expression
-        int year = 3;
-        int marks = 80;
-        switch(year) //Switch expression
+    static boolean prime(int x)
+    {
+        int divisibleCtr = 0;
+        boolean primeNumber = false;
+
+        // For loop
+        for (int i = 1; i <= x; i++)
         {
-            //Case statements
-            case 1:
-                System.out.println("First year students are not eligible for scholarship ");
-                break;
-            case 2:
-                System.out.println("Second year students are not eligible for scholarship");
-                break;
-            case 3:
-                switch(marks)
-                {
-                    case 50:
-                        System.out.println("You are not eligible for scholarship");
-                        break;
-                    case 80:
-                        System.out.println("Congrats!!!!! you are eligible for scholarship");
-                        break;
-                }
-                break;
-            //Default case statement
-            default: System.out.println("Please enter valid year");
+            if (x % i == 0)
+            {
+                // Counts all the divisible numbers
+                divisibleCtr++;
+            }
         }
 
-//        int a = 1;
-//
-//        int [][] integers = {
-//                {3, 5, 7},
-//                {10, 2, 9},
-//                {1, 3, 5}
-//        };
-//
-//        for (int i = 1; i < 3; i++)
-//        {
-//            for (int j = 1; j < integers[i].length; j++)
-//            {
-//                System.out.print(integers[i][j] + " ");
-//
-//                if (i == j)
-//                {
-//                    a *= integers[j][i];
-//                }
-//            }
-//
-//            System.out.println();
-//        }
-//        System.out.println(a);
+        // Not prime if divisible numbers are greater than 2
+        if (!(divisibleCtr > 2))
+            primeNumber = true;
+
+        return primeNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num, ctr = 0, pos = 0, primeNum = 0, max, min, x, maxPrime = 0, minPrime = 0;
+        int maxPos = 0, minPos = 0, primeCtr = 0;
+
+        num = in.nextInt();
+
+        // Checks if input value is 1 (1 is not a prime number)
+        if (num == 1)
+        {
+            System.out.println("1 is not a prime number. Please input a valid number.");
+            System.exit(0);
+        }
+
+        // Initializes max and min with num input
+        max = num;
+        min = num;
+
+        while (num != 0)
+        {
+            x = num;
+
+            // Counts all the values
+            ctr++;
+
+            // Checks if number in the series is a prime number
+            if (prime(x))
+            {
+                // Position of the prime number
+                pos = ctr;
+                // The prime number
+                primeNum = x;
+
+                // Will get the max and min values only if the number prime
+                // Checks also if the max or min value is prime or not
+                if (max < primeNum || (!(prime(max))))
+                {
+                    max = primeNum;
+                    maxPos = pos; // Position of the max prime
+                }
+                if (min > primeNum || (!(prime(min))))
+                {
+                    min = primeNum;
+                    minPos = pos; // Position of the min prime
+                }
+
+                // Max and min prime numbers
+                maxPrime = max;
+                minPrime = min;
+
+                // Counts the total number of prime numbers
+                primeCtr++;
+            }
+
+            // Gets another input
+            num = in.nextInt();
+        }
+
+        // If count of prime is 1
+        if (primeCtr == 1)
+        {
+            System.out.println("There is only prime number, the prime number is " + primeNum + ", position is " + pos);
+            System.exit(0);
+        }
+        // If there is no prime numbers in the series
+        if (primeCtr == 0)
+        {
+            System.out.println("There is no prime number in the series.");
+            System.exit(0);
+        }
+
+        // Outputs
+        System.out.println();
+        System.out.println("Highest prime number is " + maxPrime + ", position is " + maxPos);
+        System.out.println("Lowest prime number is " + minPrime + ", position is " + minPos);
     }
 }
---
+++ Test/Test2.java
@@ -0,0 +1,22 @@
+/*Java Program to create a jagged array which contains two 1D array*/
+package Test;
+import java.util.*;
+
+class EmptyClass
+{
+
+    public void testPrint()
+    {
+        System.out.println("Print");
+    }
+}
+
+public class Test2
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
