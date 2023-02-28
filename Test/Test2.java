f0d71685238ec8c133e21eb9f21c670dbfecc742
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-02-28 08:36:09 +0800

    Updated

--- Test/Test2.java
+++ Test/Test2.java
@@ -1,22 +1,19 @@
-/*Java Program to create a jagged array which contains two 1D array*/
 package Test;
-import java.util.*;
-
-class EmptyClass
-{
-
-    public void testPrint()
-    {
-        System.out.println("Print");
-    }
-}
 
 public class Test2
 {
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Test2 w = new Test2(); Test2 w2 = new Test2();
+        w2 = go(w, w2);
 
+        System.out.println(w2 == w);
 
     }
+    static Test2 go(Test2 wr1, Test2 wr2)
+    {
+        Test2 wr3 = wr1; wr1 = wr2; wr2 = wr3;
+
+        return wr3;
+    }
+
 }
