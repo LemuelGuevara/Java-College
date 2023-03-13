6d672aa9de09b71a1ec754a6701f7b60602d4342
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-03-13 08:22:16 +0800

    Revised MACHINE PROBLEM 02 (2nd SEM)

--- 1st Year/2nd Semester/MachineProblems/MP2Java2.java
+++ 1st Year/2nd Semester/MachineProblems/MP2Java2.java
@@ -9,32 +9,40 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // Enums for getting unit information such as areas and rates
-enum UnitInformation
+enum UnitEnums
 {
+    // BEDROOM-COUNT_BEDROOM(unitArea, leseUnitRate, ownerUnitRate)
+
     // The unit areas
-    ONE_BEDROOM(30), TWO_BEDROOMS(65), THREE_BEDROOMS(96),
-    // The rates of the units
-    ONE_BEDROOM_LESSE(89.50), TWO_BEDROOM_LESSE(95.75), THREE_BEDROOM_LESSE(102.80),
-    ONE_BEDROOM_OWNER(85.75), TWO_BEDROOM_OWNER(89.50), THREE_BEDROOM_OWNER(93.25);
+    ONE_BEDROOM("1-Bedroom", 30, 89.50, 85.75, 1),
+    TWO_BEDROOM("2-Bedroom",65, 95.75, 89.50, 2),
+    THREE_BEDROOM("3-Bedroom",96, 102.80, 93.25, 3);
 
-    private final double value;
+    private final double unitArea, lesseUnitRate, ownerUnitRate;
+    private final int numberOfParking;
+    private final String unitType;
 
-    UnitInformation(final double unitInformation) {value = unitInformation;}
-    public double getUnitArea() {return value;}
-}
+    UnitEnums(String unitType, double unitArea, double lesseUnitRate, double ownerUnitRate, int numberOfParking)
+    {
+        this.unitType = unitType;
+        this.unitArea = unitArea;
+        this.lesseUnitRate = lesseUnitRate;
+        this.ownerUnitRate = ownerUnitRate;
+        this.numberOfParking = numberOfParking;
+    }
 
-// Interface that calculates the dues
-interface AssociationDueCalculator
-{
-    double getTotalArea(double unitArea);
-    double getMonthlyAssoctDues(double totalArea, double unitRate);
+    public double getUnitAreaEnum() {return unitArea;}
+    public double getLesseUnitRateEnum() {return lesseUnitRate;}
+    public double getOwnerUnitRateEnum() {return ownerUnitRate;}
+    public int getNumberOfParkingEnum() {return numberOfParking;}
+    public String getUnitTypeEnum() {return unitType;}
 }
 
 // Interface for storing error logs
 interface ExceptionErrorLogs
 {
-    final String INTEGER_ERROR_LOG = "Integer should be only numeric and positive";
-    final String CHAR_ERROR_LOG = "Char should not be numeric";
+    String INTEGER_ERROR_LOG = "Integer should be only numeric and positive";
+    String CHAR_ERROR_LOG = "Char should not be numeric";
 }
 
 // A class made for exception handling values
@@ -42,6 +50,12 @@ class ExceptionHandlers implements ExceptionErrorLogs
 {
     static Scanner in = new Scanner(System.in);
     ExceptionHandlers() {}
+
+    private void exceptionErrorLog(String errorType)
+    {
+        System.out.println(errorType);
+        System.exit(0);
+    }
 
     // Function that gets a valid integer then validates
     public int validIntegerInput()
@@ -54,14 +68,12 @@ class ExceptionHandlers implements ExceptionErrorLogs
 
             if (userInput < 0)
             {
-                System.out.println(INTEGER_ERROR_LOG);
-                System.exit(0);
+                exceptionErrorLog(INTEGER_ERROR_LOG);
             }
         }
         catch (InputMismatchException e)
         {
-            System.out.println(INTEGER_ERROR_LOG);
-            System.exit(0);
+            exceptionErrorLog(INTEGER_ERROR_LOG);
         }
 
         return userInput;
@@ -76,168 +88,191 @@ class ExceptionHandlers implements ExceptionErrorLogs
             charInput = in.next().charAt(0);
 
             if (Character.isDigit(charInput))
-            {
-                System.out.println("Your input should a character");
-                System.exit(0);
-            }
+                exceptionErrorLog(CHAR_ERROR_LOG);
         }
         catch (RuntimeException e)
         {
-            System.out.println(CHAR_ERROR_LOG);
-            System.exit(0);
+           exceptionErrorLog(CHAR_ERROR_LOG);
         }
 
-        return charInput;
+        return Character.toUpperCase(charInput);
     }
 }
 
-// The class responsible for getting the dues and displaying it
-class UnitDues implements AssociationDueCalculator
+class UnitInformation
 {
-    private final double PARKING_AREA_SPACE = 12.5;
-    protected int unitType, numberOfParking;
-    protected final char occupantType, paymentOption;
+    static ExceptionHandlers validInput = new ExceptionHandlers();
 
-    UnitDues(int unitType, char occupantType, int numberOfParking, char paymentOption)
+    private UnitEnums unitTypeEnum;
+    protected char occupantType, paymentOption;
+    protected int numberOfParking;
+
+    UnitInformation(char unitType, char occupantType, int numberOfParking, char paymentOption)
     {
-        this.unitType = unitType;
+        switch (unitType)
+        {
+            case 'A': this.unitTypeEnum = UnitEnums.ONE_BEDROOM; break;
+            case 'B': this.unitTypeEnum = UnitEnums.TWO_BEDROOM; break;
+            case 'C': this.unitTypeEnum = UnitEnums.THREE_BEDROOM; break;
+        }
+
         this.occupantType = occupantType;
         this.numberOfParking = numberOfParking;
         this.paymentOption = paymentOption;
     }
 
+    public String getUnitType()
+    {
+        return unitTypeEnum.getUnitTypeEnum();
+    }
+
+    public double getUnitArea()
+    {
+        return unitTypeEnum.getUnitAreaEnum();
+    }
+
+    public double getUnitRate()
+    {
+        double unitRate = 0;
+
+        if (occupantType == 'L')
+            unitRate = unitTypeEnum.getLesseUnitRateEnum();
+        if (occupantType == 'O')
+            unitRate = unitTypeEnum.getOwnerUnitRateEnum();
+
+        return unitRate;
+    }
+
+    public double getUnitNumberOfParking()
+    {
+        String PARKING_SPACES_ERROR_LOG = "You have reached the maximum number of parking spaces. Please input again:";
+
+        /*for (numberOfParking = validInput.validIntegerInput(); numberOfParking != unitTypeEnum.getNumberOfParking();
+             numberOfParking = validInput.validIntegerInput())
+            System.out.println(PARKING_SPACES_ERROR_LOG);
+         */
+
+        return numberOfParking;
+    }
+
+    public char getPaymentOption()
+    {
+        return paymentOption;
+    }
+}
+
+class UnitDueCalculator
+{
+    UnitInformation unitInformation;
+
+    private double totalArea, monthlyDue, yearlyDue;
+
+    UnitDueCalculator(UnitInformation unitInformation)
+    {
+        this.unitInformation = unitInformation;
+    }
+
     // Gets the total area
-    @Override
-    public double getTotalArea(double unitArea) {
-        double totalArea;
+    public void getTotalArea()
+    {
+        double parkingAreaSpace = 12.5;
 
-        totalArea = (numberOfParking * PARKING_AREA_SPACE) + unitArea;
-
-        return totalArea;
+        totalArea = (unitInformation.getUnitNumberOfParking() * parkingAreaSpace) + unitInformation.getUnitArea();
     }
 
     // Gets the monthly association dues
-    @Override
-    public double getMonthlyAssoctDues(double totalArea, double unitRate)
+
+    public void getMonthlyDues()
     {
-        return totalArea * unitRate;
+        monthlyDue = totalArea * unitInformation.getUnitRate();
     }
 
-    // Evaluates the choice of the user
-    public void evaluateChoice()
+    public void getYearlyDues()
     {
-        double unitArea = 0, unitRate = 0, totalArea, monthlyDue = 0, yearlyDue;
-
-        // Switch case for getting the unit type and its details
-        switch (unitType)
-        {
-            case 1:
-                unitArea = UnitInformation.ONE_BEDROOM.getUnitArea();
-
-                if (occupantType == 'L')
-                    unitRate = UnitInformation.ONE_BEDROOM_LESSE.getUnitArea();
-                if (occupantType == 'O')
-                    unitRate = UnitInformation.ONE_BEDROOM_OWNER.getUnitArea();
-                break;
-            case 2:
-                unitArea = UnitInformation.TWO_BEDROOMS.getUnitArea();
-
-                if (occupantType == 'L')
-                    unitRate = UnitInformation.TWO_BEDROOM_LESSE.getUnitArea();
-                if (occupantType == 'O')
-                    unitRate = UnitInformation.TWO_BEDROOM_OWNER.getUnitArea();
-                break;
-            case 3:
-                unitArea = UnitInformation.THREE_BEDROOMS.getUnitArea();
-
-                if (occupantType == 'L')
-                    unitRate = UnitInformation.THREE_BEDROOM_LESSE.getUnitArea();
-                if (occupantType == 'O')
-                    unitRate = UnitInformation.THREE_BEDROOM_OWNER.getUnitArea();
-                break;
-        }
-
-        // Computes the dues
-        totalArea = getTotalArea(unitArea);
-        monthlyDue = getMonthlyAssoctDues(totalArea, unitRate);
         yearlyDue = monthlyDue * 11;
+    }
 
+    public double getTotalDue()
+    {
+        double totalDue = 0;
+
+        getTotalArea();
+        getMonthlyDues();
+        getYearlyDues();
+
+        if (unitInformation.getPaymentOption() == 'M')
+            totalDue = monthlyDue;
+        if (unitInformation.getPaymentOption() == 'Y')
+            totalDue = yearlyDue;
+
+        return totalDue;
+    }
+}
+
+class UnitPaymentDetails extends UnitDueCalculator
+{
+    UnitPaymentDetails(UnitInformation unitInformation)
+    {
+        super(unitInformation);
+    }
+
+    public void showPaymentDetails()
+    {
         // Shows the due details
-        System.out.println("\nUnity Type: " + unitType + "-Bedroom");
+        System.out.println("\nUnity Type: " + unitInformation.getUnitType());
 
-        if (occupantType == 'L')
+        if (unitInformation.occupantType == 'L')
             System.out.println("Occupant Type: Lesse");
-        if (occupantType == 'O')
+        if (unitInformation.occupantType == 'O')
             System.out.println("Occupant Type: Owner");
-        System.out.println("Number of Parking: " + numberOfParking);
 
-        if (paymentOption == 'M')
-        {
-            System.out.println("Payment Option: Monthly");
-            System.out.printf("Total Due: %.2f", monthlyDue);
-        }
-        if (paymentOption == 'Y')
-        {
-            System.out.println("Payment Option: Yearly");
-            System.out.printf("Total Due: %.2f", yearlyDue);
-        }
-        System.out.println();
+        System.out.println("Number of Parking: " + unitInformation.numberOfParking);
+        System.out.println("Total Due: " + getTotalDue());
     }
 }
 
 public class MP2Java2
 {
-    static Scanner in = new Scanner(System.in);
-    static ExceptionHandlers validInput = new ExceptionHandlers();
-
-    // Checks if the number of parking space is within range
-    public static int validChoiceRange()
-    {
-        int numberOfParking;
-
-        for(numberOfParking = validInput.validIntegerInput(); numberOfParking < 0 || numberOfParking > 3; numberOfParking = validInput.validIntegerInput())
-            System.out.print("Input is not from any of the choices, please input again:");
-
-        return numberOfParking;
-    }
-
     // Checks if the program is continuous
     public static boolean isContinuous(char userChoice)
     {
-        boolean isContinuity = false;
-
-        if (userChoice == 'Y')
-            isContinuity = true;
+        boolean isContinuity = userChoice == 'Y';
 
         return isContinuity;
     }
 
     public static void main(String[] args)
     {
-        UnitDues unitDues;
-        int unitType, numberOfParking;
-        char occupantType, paymentOption, userChoice;
+        ExceptionHandlers validInput = new ExceptionHandlers();
+        UnitInformation unitInformation;
+        UnitPaymentDetails unitPaymentDetails;
+
+        int numberOfParking;
+        char unitType, occupantType, paymentOption, userChoice;
 
         do
         {
             System.out.println("\nThis program will calculate the association dues(AD) an occupant(Owner or Lessee) has to pay");
-            System.out.println("Enter unit type (1-Bedroom, 2-Bedroom, 3-Bedroom):");
-            unitType = validChoiceRange();
+            System.out.println("Enter unit type (A = 1-Bedroom, B = 2-Bedroom, C = 3-Bedroom):");
+            unitType = validInput.validCharInput();
+
             System.out.println("Enter occupant Type (L-Lesse, O-Owner): ");
             occupantType = validInput.validCharInput();
-            occupantType = Character.toUpperCase(occupantType);
+
             System.out.println("Enter number of parking (Max is 3):");
-            numberOfParking = validChoiceRange();
+            numberOfParking = validInput.validIntegerInput();
+
             System.out.println("Enter payment option (M - Monthly, Y - Yearly: ");
             paymentOption = validInput.validCharInput();
             paymentOption = Character.toUpperCase(paymentOption);
 
-            unitDues = new UnitDues(unitType, occupantType, numberOfParking, paymentOption);
-            unitDues.evaluateChoice();
+            unitInformation = new UnitInformation(unitType, occupantType, numberOfParking, paymentOption);
+            unitPaymentDetails = new UnitPaymentDetails(unitInformation);
+            unitPaymentDetails.showPaymentDetails();
 
             // Asks the user if they still want to continue the program
             System.out.println("\nPress Y to continue and press any other key to stop.");
-            userChoice = in.next().charAt(0);
+            userChoice = validInput.validCharInput();
             userChoice = Character.toUpperCase(userChoice);
 
         } while (isContinuous(userChoice));
